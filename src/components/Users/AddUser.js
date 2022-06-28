298caef2b0a181d2616d196a536d1de643b347ad
Radu Baroncea <baroncea.radu@gmail.com>
2022-06-28 17:41:49 +0300

    add state management

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
+
+  const usernameHandler = (event) => setEnteredUsername(event.target.value);
+  const ageHandler = (event) => setEnteredAge(event.target.value);
 
   const onClick = () => {};
 
@@ -15,9 +22,9 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageHandler} />
         <Button type="submit" handleClick={onClick}>
           Add User
         </Button>
